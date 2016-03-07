f8a6bfe4daa8e473ea6a8efb6d2521e31167e002
tka1 <tomi.j.kallio@kolumbus.fi>
2016-03-07 19:45:51 +0200

    added adif reader and twitter export

---
+++ AdifReader/AdifReader/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Npgsql;
+
+namespace AdifReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dbserver = System.Configuration.ConfigurationManager.AppSettings["dbserver"];
+            string database = System.Configuration.ConfigurationManager.AppSettings["database"];
+            string userid = System.Configuration.ConfigurationManager.AppSettings["userid"];
+            string password = System.Configuration.ConfigurationManager.AppSettings["password"];
+            NpgsqlConnection conn = new NpgsqlConnection("Server=" + dbserver + ";User Id=" + userid + ";Password=Saturnus1!" + ";Database=" + database + ";");
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("OH2BBT.adi"))
+                {
+                    string line;
+
+                    // Read and display lines from the file until 
+                    // the end of the file is reached. 
+                    string callsign = "";
+                    string mode = "";
+                    string band = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //string adif = " < CALL:6 > EA3HKA < QSO_DATE:8 > 20150308 < TIME_ON:6 > 153513 < BAND:3 > 10M < STATION_CALLSIGN:6 > OH2BBT < FREQ:8 > 28.09189 < CONTEST_ID:13 > DARC - 10M - DIGI < FREQ_RX:8 > 28.09189 < MODE:4 > RTTY < RST_RCVD:3 > 599 < RST_SENT:3 > 001 < OPERATOR:6 > OH2BBT < SRX:1 > 1 < CQZ:2 > 14 < STX:1 > 1 < APP_N1MM_POINTS:1 > 1 < APP_N1MM_RADIO_NR:1 > 1 < APP_N1MM_CONTINENT:2 > EU < APP_N1MM_RUN1RUN2:1 > 1 < APP_N1MM_RADIOINTERFACED:1 > 1 < APP_N1MM_ISORIGINAL:4 > True < APP_N1MM_NETBIOSNAME:5 > INTEL < APP_N1MM_ISRUNQSO:1 > 0 < EOR >";
+                        string[] x = Regex.Split(line.Replace("\n", "").Replace("\r", ""), @"<(.*?):.*?>([^<\t\n\r\f\v]+)").Where(S => !string.IsNullOrEmpty(S)).ToArray();
+
+                        int callid = Array.IndexOf(x, "CALL");
+                        if (callid != -1)
+                        {
+                             callsign = x[callid + 1];
+                            Console.WriteLine(callsign.Trim());
+                        }
+
+                        int bandid = Array.IndexOf(x, "BAND");
+                        if (callid != -1) {
+                             band = x[bandid + 1];
+                            Console.WriteLine(band.Trim());
+                        }
+
+                        int modeid = Array.IndexOf(x, "MODE");
+                        if (callid != -1)
+                        {
+                             mode = x[modeid + 1];
+                            Console.WriteLine(mode.Trim());
+                        }
+
+                        conn.Open();
+                        NpgsqlCommand cmd2 = new NpgsqlCommand("SELECT  country,continent,length(prefix) FROM cluster.dxcc where :value1 like concat(dxcc.prefix, '%') order by 3 desc limit 1", conn);
+                        cmd2.Parameters.Add(new NpgsqlParameter("value1", NpgsqlTypes.NpgsqlDbType.Text));
+                        cmd2.Parameters[0].Value = callsign;
+                        // string dxcountry = (String)cmd2.ExecuteScalar();
+                        NpgsqlDataReader dr2 = cmd2.ExecuteReader();
+                        string dxcountry = "";
+                        //string dx_cont = "";
+                        while (dr2.Read())
+                        {
+                            dxcountry = dr2[0].ToString();
+                           // dx_cont = dr2[1].ToString();
+                        }
+                        conn.Close();
+                        Console.WriteLine(dxcountry);
+                        conn.Open();
+
+                        NpgsqlCommand cmd = new NpgsqlCommand("insert into cluster.log(call,country,mode,band) values ( :value1 ,:value2,:value3,:value4)", conn);
+                        cmd.Parameters.Add(new NpgsqlParameter("value1", NpgsqlTypes.NpgsqlDbType.Text));
+                        cmd.Parameters.Add(new NpgsqlParameter("value2", NpgsqlTypes.NpgsqlDbType.Text));
+                        cmd.Parameters.Add(new NpgsqlParameter("value3", NpgsqlTypes.NpgsqlDbType.Text));
+                        cmd.Parameters.Add(new NpgsqlParameter("value4", NpgsqlTypes.NpgsqlDbType.Text));
+
+                        cmd.Parameters[0].Value = callsign.Trim();
+                        cmd.Parameters[1].Value = dxcountry.Trim();
+                        cmd.Parameters[2].Value = mode.Trim();
+                        cmd.Parameters[3].Value = band.Trim();
+
+                        NpgsqlDataReader dr = cmd.ExecuteReader();
+                        conn.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+    
+
+
+        }
+
+    }
+
---
+++ ClusterAlarm/ClusterAlarm/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToTwitter;
+using Npgsql;
+
+
+namespace ClusterAlarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dbserver = System.Configuration.ConfigurationManager.AppSettings["dbserver"];
+            string database = System.Configuration.ConfigurationManager.AppSettings["database"];
+            string userid = System.Configuration.ConfigurationManager.AppSettings["userid"];
+            string password = System.Configuration.ConfigurationManager.AppSettings["password"];
+            NpgsqlConnection conn = new NpgsqlConnection("Server=" + dbserver + ";User Id=" + userid + ";Password=Saturnus1!" + ";Database=" + database + ";");
+            const string accessToken = "706060410919768064-CZeVKheTbkIPnuwcr0Qrt5wDy0SODWm";
+            const string accessTokenSecret = "QDmECT5sUDxVUKdx557EfCsZYAMgIYCY8zuyLPyqH68fa";
+            const string consumerKey = "3cH1GIp17dOnNQl9M3X9Mh4b9";
+            const string consumerSecret = "8sY3HShekmRux4vvhC5gyqpwXDBehYup1Fsd8K53jBrDVZhOfB";
+            const string twitterAccountToDisplay = "kalliotj";
+            string title = "";
+            string time = "";
+            string call = "";
+            string freq = "";
+            string band = "";
+            string country = "";
+            string mode = "";
+
+            var authorizer = new SingleUserAuthorizer
+            {
+                CredentialStore = new InMemoryCredentialStore
+                {
+                    ConsumerKey = consumerKey,
+                    ConsumerSecret = consumerSecret,
+                    OAuthToken = accessToken,
+                    OAuthTokenSecret = accessTokenSecret
+                }
+            };
+            while (true)
+            {
+                try
+
+                {
+                    conn.Open();
+                    NpgsqlCommand cmd2 = new NpgsqlCommand("SELECT  distinct title,now(), dxcall, freq, band, country, mode, dx_continent,skimmode FROM cluster.alert", conn);
+                    //cmd2.Parameters.Add(new NpgsqlParameter("value1", NpgsqlTypes.NpgsqlDbType.Text));
+                    // cmd2.Parameters[0].Value = callsign;
+                    // string dxcountry = (String)cmd2.ExecuteScalar();
+                    NpgsqlDataReader dr2 = cmd2.ExecuteReader();
+
+                    //string dx_cont = "";
+                    while (dr2.Read())
+                    {
+                        title = dr2[0].ToString();
+                        time = dr2[1].ToString();
+                        call = dr2[2].ToString();
+                        freq = dr2[3].ToString();
+                        band = dr2[4].ToString();
+                        country = dr2[5].ToString();
+                        mode = dr2[6].ToString();
+                        Console.WriteLine(title + " " + time + " " + call + " " + freq + " " + band + " " + mode + " " + country);
+                        using (var context = new TwitterContext(authorizer))
+                        {
+                            if (call != "")
+                            {
+
+                                  var tweet = context.TweetAsync(title + " " + time + " " + call + " " + freq + " " + band + " " + mode + " " + country).Result;
+                            }
+                        };
+                    }
+
+                   
+                    conn.Close();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+          
+               /* try
+                {
+                   
+
+                }
+                catch (Exception e)
+                { Console.WriteLine(e); }
+                */
+                //  Console.ReadLine();
+                System.Threading.Thread.Sleep(59000);
+            }
+
+        }
+
+    }
+}
